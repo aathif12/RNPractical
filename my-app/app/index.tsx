e9a57c76d73ae011ad9870b3d0aedae63d4ef620
aathif12 <aathifahamad4@gmail.com>
2024-12-31 14:10:46 +0530

    Components

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -1,15 +1,16 @@
 import { Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
+    <SafeAreaProvider
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      
+    </SafeAreaProvider>
   );
 }
